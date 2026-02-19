2eabfe8afbed408beec03b6957d55adb5bf82671
rscwn <96673832+rscwn@users.noreply.github.com>
2026-02-19 13:35:47 -0800

    Add files via upload

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,16 +1,47 @@
 #!/usr/bin/env bash
 
 # user install script
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/rscwn/testing-nn10/refs/heads/main/Mac/install.sh)"
 
 ARCH=$(uname -m)
 
 JOLY_BIN_URL="https://raw.githubusercontent.com/rscwn/testing-nn10/refs/heads/main/Mac/"
 INSTALLER_BIN_URL="$JOLY_BIN_URL/instwaller-$ARCH"
 
+# we need to check if we already installed
+BUNDLE_DIR="RonixM.app"
+APP_DIR="/Applications/$BUNDLE_DIR"
+
+UPDATE=0
+if [ -d "$APP_DIR" ]; then
+    echo "RonixM is already installed in /Applications. Do you want to update or reinstall?"
+    echo "1) Update"
+    echo "2) Reinstall"
+    echo "3) Exit"
+
+    read -r CHOICE
+    case $CHOICE in
+        1)
+            UPDATE=1
+            ;;
+        2)
+            rm -rf "$APP_DIR"
+            ;;
+        3)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Exiting..."
+            exit 1
+            ;;
+    esac
+fi
+
 # now we donwload an run
 curl -L "$INSTALLER_BIN_URL" -o ./instwaller
 chmod +x ./instwaller
-./instwaller
+./instwaller $UPDATE
 
 # now we don
 rm ./instwaller
